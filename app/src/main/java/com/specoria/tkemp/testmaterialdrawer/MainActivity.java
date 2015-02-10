34e5bd1b99c684420194e7206e7e372d8a4eb615
Tyson Kemp <processnotproduct>
2015-02-09 23:05:30 -0500

    adding the header to the drawer

--- app/src/main/java/com/specoria/tkemp/testmaterialdrawer/MainActivity.java
+++ app/src/main/java/com/specoria/tkemp/testmaterialdrawer/MainActivity.java
@@ -1,21 +1,37 @@
 package com.specoria.tkemp.testmaterialdrawer;
 
+import android.support.v4.app.FragmentManager;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.squareup.picasso.Picasso;
 
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private FragmentManager mFragmentManager;
+    private View mHeaderView;
+
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mResolvingError = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +41,11 @@ public class MainActivity extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
+        mHeaderView = getLayoutInflater().inflate(R.layout.user_header_layout, null);
         mFragmentManager = getSupportFragmentManager();
+
+        mGoogleApiClient = buildGoogleApiClient();
+        mGoogleApiClient.connect();
 
         mDrawerList = (ListView) findViewById(R.id.drawer_listview);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -33,7 +53,22 @@ public class MainActivity extends ActionBarActivity {
                 R.string.open, R.string.close);
         mDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerList.addHeaderView(mHeaderView);
 
+        // set adapter
+        // set onitemclicklistener
+
+    }
+
+    //item click listener
+
+    public GoogleApiClient buildGoogleApiClient() {
+        return new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
     }
 
     @Override
@@ -56,5 +91,89 @@ public class MainActivity extends ActionBarActivity {
         }
 
         super.onBackPressed();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                // start and activity to try and fix the login error
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // try logging in again
+                mGoogleApiClient.connect();
+            }
+        } else {
+            mResolvingError = true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // response from the activity launched by login to get user input
+        // to successfully log in
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            mResolvingError = false;
+            if (resultCode == RESULT_OK) {
+                if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // get current users account
+        Person user = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+        if (user != null) {
+            // if the user has a profile image
+            if (user.getImage().hasUrl()) {
+                String portrait = user.getImage().getUrl();
+                //load into the portrait imageview
+                Picasso.with(this)
+                        //remove parameters since the image url makes the image really small
+                        .load(portrait.split("\\?")[0])
+                        .into((ImageView) mHeaderView.findViewById(R.id.user_image));
+            }
+
+            // load the banner into the background
+            Picasso.with(this)
+                    .load(user.getCover().getCoverPhoto().getUrl())
+                    .into((ImageView) mHeaderView.findViewById(R.id.banner_image));
+
+            // set the account name
+            ((TextView) findViewById(R.id.user_account))
+                    .setText(Plus.AccountApi.getAccountName(mGoogleApiClient));
+
+            // set the user's name
+            Person.Name userName = user.getName();
+            ((TextView) findViewById(R.id.user_name))
+                    .setText(String.format("%s %s", userName.getGivenName(), userName.getFamilyName()));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 }
